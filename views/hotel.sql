0698d0eceab073ef4ed8991cdb7e98709e5874e0
coderyourfuture <you@example.com>
2018-02-04 15:25:14 +0000

    .

---
+++ views/hotel.sql
@@ -0,0 +1,7 @@
+create table customers (
+    title varchar,
+    firstname varchar,
+    surname varchar
+);
+
+insert into customers (title, firstname, surname, email) values ('Mr', 'Donald', 'Trump');
